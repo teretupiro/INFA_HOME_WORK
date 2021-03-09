e553a8b366a2dfdb6a2671cf8feee599eb138161
unknown <vafin-2005@list.ru>
2021-03-09 20:13:58 +0300

    NOK and NOD

---
+++ НОК И НОД.pas
@@ -0,0 +1,16 @@
+﻿var 
+  a,b :integer;
+  function nod (a,b:integer): integer;
+  begin
+    while a*b <>0 do
+      if a>b then
+        a:=a mod b 
+      else 
+        b:=b mod a;
+      nod:=a+b;
+      
+  end;
+  begin
+    readln(a,b);
+    write (a*b / nod (a,b));
+  end.
